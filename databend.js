715b0687fec3ba32a736cfaffda82d160878b1d0
Michael Vattuone <mvattuone@gmail.com>
2017-10-01 11:11:57 -0400

    Add ability to have dynamic framerate for slowing down video for complex effects

--- databend.js
+++ databend.js
@@ -2,194 +2,177 @@
     var effects = require('./effects.json');
 
     // Create a Databender instance
-    module.exports = function (image, audioCtx) {
-      var _this = this;
-
+    module.exports = function (audioCtx, channels) {
       this.effects = effects;
 
       // Create an AudioContext or use existing one
       this.audioCtx = audioCtx ? audioCtx : new AudioContext();
+      
+      this.channels = channels ? channels : 1;
 
-      // Create in-memory (i.e. non-rendered canvas for getting Image Data)
-      var canvas = this.canvas = document.createElement('canvas');
-      canvas.width = image.width;
-      canvas.height= image.height;
+      this.bend = function (imageData) {
+        this.imageData = imageData;
+        var bufferSize = imageData.data.length / this.channels;
 
-      this.channels = 1;
-      var ctx = this.ctx = canvas.getContext('2d');
+        // Make an audioBuffer on the audioContext to pass to the offlineAudioCtx AudioBufferSourceNode
+        var audioBuffer = this.audioCtx.createBuffer(this.channels, bufferSize, this.audioCtx.sampleRate);
 
-      // Draw image to canvas and get image data
-      ctx.putImageData(image, 0, 0);
-      this.imageData = ctx.getImageData(0, 0, image.width, image.height).data;
+        // This gives us the actual ArrayBuffer that contains the data
+        var nowBuffering = audioBuffer.getChannelData(0);
 
-      this.bufferSize = this.imageData.length / this.channels;
+        // set the AudioBuffer buffer to the same as the imageData audioBuffer
+        // v. convenient becuase you do not need to convert the data yourself
+        nowBuffering.set(imageData.data);
 
-      // Make an audioBuffer on the audioContext to pass to the offlineAudioCtx AudioBufferSourceNode
-      this.audioBuffer = this.audioCtx.createBuffer(this.channels, this.bufferSize, this.audioCtx.sampleRate);
-
-      // This gives us the actual ArrayBuffer that contains the data
-      this.nowBuffering = this.audioBuffer.getChannelData(0);
-
-
-      // set the AudioBuffer buffer to the same as the imageData audioBuffer
-      // v. convenient becuase you do not need to convert the data yourself
-      this.nowBuffering.set(this.imageData);
-
-      this.setParam = function (effect, paramName, paramValue)  {
-        this.effects[effect][paramName] = paramValue;
-        return this.render(this.imageData);
+        return this.render(audioBuffer);
       }
 
       this.toggleEffect = function (effect, params) {
         this.effects[effect].active = !this.effects[effect].active
-        return this.render(this.imageData);
+        return this.render(audioBuffer);
       }
 
       this.setEffect = function (effect) {
         this.effects[effect].active = true
-        return this.render(this.imageData);
+        return this.render(audioBuffer);
       }
 
       this.unsetEffect = function (effect) {
         effects[effect].active = false
-        return this.render(this.imageData);
+        return this.render(audioBuffer);
       }
 
-      this.render = function (data) {
-        var _this = this;
-        var effects = this.effects;
+      this.render = function (buffer) {
+        return new Promise(function (resolve, reject) {
+          var _this = this;
+          var effects = this.databender.effects;
 
-        // Create offlineAudioCtx that will house our rendered buffer
-        var offlineAudioCtx = new OfflineAudioContext(this.channels, this.bufferSize, this.audioCtx.sampleRate);
+          // Create offlineAudioCtx that will house our rendered buffer
+          var offlineAudioCtx = new OfflineAudioContext(this.databender.channels, buffer.length, this.audioCtx.sampleRate);
 
-        // Create an AudioBufferSourceNode, which represents an audio source consisting of in-memory audio data
-        var bufferSource = offlineAudioCtx.createBufferSource();
+          // Create an AudioBufferSourceNode, which represents an audio source consisting of in-memory audio data
+          var bufferSource = offlineAudioCtx.createBufferSource();
 
-        // Set buffer to audio buffer containing image data
-        bufferSource.buffer = this.audioBuffer;
+          // Set buffer to audio buffer containing image data
+          bufferSource.buffer = buffer; 
 
-        // Seems to rotate the image, clockwise if postitive ccw if negative
-        if (effects.detune.active) {
-          var noEffects = false;
-          bufferSource.detune.value = effects.detune.value;
-        }
+          // Seems to rotate the image, clockwise if postitive ccw if negative
+          if (effects.detune.active) {
+            var noEffects = false;
+            bufferSource.detune.value = effects.detune.value;
+          }
 
-        // Seems to "play back" the image at a rate equal to the number
-        // (i.e. 4 yields 4 smaller rendered images)
-        if (effects.playbackRate.active) {
-          var noEffects = false;
-          bufferSource.playbackRate.value = effects.playbackRate.value;
-        }
+          // Seems to "play back" the image at a rate equal to the number
+          // (i.e. 4 yields 4 smaller rendered images)
+          if (effects.playbackRate.active) {
+            var noEffects = false;
+            bufferSource.playbackRate.value = effects.playbackRate.value;
+          }
 
-        //  @NOTE: Calling this is when the AudioBufferSourceNode becomes unusable
-        bufferSource.start();
+          //  @NOTE: Calling this is when the AudioBufferSourceNode becomes unusable
+          bufferSource.start();
 
-        // @TODO Can we decouple effects from this?
-        var noEffects = true;
-        var tuna = new Tuna(offlineAudioCtx);
+          // @TODO Can we decouple effects from this?
+          var noEffects = true;
+          var tuna = new Tuna(offlineAudioCtx);
 
+          if (effects.bitcrusher.active) {
+            var noEffects = false;
 
-        if (effects.bitcrusher.active) {
-          var noEffects = false;
+            var crusher = new tuna.Bitcrusher({
+                bits: effects.bitcrusher.bits,
+                normfreq: effects.bitcrusher.normfreq,
+                bufferSize: effects.bitcrusher.bufferSize
+            });
+            bufferSource.connect(crusher);
+            crusher.connect(offlineAudioCtx.destination);
+          }
 
-          var crusher = new tuna.Bitcrusher({
-              bits: effects.bitcrusher.bits,
-              normfreq: effects.bitcrusher.normfreq,
-              bufferSize: effects.bitcrusher.bufferSize
-          });
-          bufferSource.connect(crusher);
-          crusher.connect(offlineAudioCtx.destination);
-        }
+          if (effects.biquad.active) {
+            var noEffects = false;
+            var biquadFilter = offlineAudioCtx.createBiquadFilter();
+            biquadFilter.type = effects.biquad.type;
+            biquadFilter.frequency.value = effects.biquad.biquadFrequency;
+            biquadFilter.gain.value = effects.biquad.biquadGain;
+            bufferSource.connect(biquadFilter);
+            biquadFilter.connect(offlineAudioCtx.destination);
+          }
 
-        if (effects.biquad.active) {
-          var noEffects = false;
-          var biquadFilter = offlineAudioCtx.createBiquadFilter();
-          biquadFilter.type = effects.biquad.type;
-          biquadFilter.frequency.value = effects.biquad.biquadFrequency;
-          biquadFilter.gain.value = effects.biquad.biquadGain;
-          bufferSource.connect(biquadFilter);
-          biquadFilter.connect(offlineAudioCtx.destination);
-        }
+          if (effects.gain.active) {
+            var noEffects = false;
+            var gainNode = offlineAudioCtx.createGain();
+            bufferSource.connect(gainNode);
+            gainNode.gain.value = effects.gain.value;
+            gainNode.connect(offlineAudioCtx.destination);
+          }
 
+          if (effects.pingPong.active) {
+            var noEffects = false;
 
-        if (effects.gain.active) {
-          var noEffects = false;
-          var gainNode = offlineAudioCtx.createGain();
-          bufferSource.connect(gainNode);
-          gainNode.gain.value = effects.gain.value;
-          gainNode.connect(offlineAudioCtx.destination);
-        }
+            var pingPongDelayNode = new tuna.PingPongDelay({
+                wetLevel: effects.pingPong.wetLevel,
+                feedback: effects.pingPong.feedback,
+                delayTimeLeft: effects.pingPong.delayTimeLeft,
+                delayTimeRight: effects.pingPong.delayTimeRight
+            });
+            bufferSource.connect(pingPongDelayNode);
+            pingPongDelayNode.connect(offlineAudioCtx.destination);
+          }
 
+          if (effects.phaser.active) {
+            var noEffects = false;
 
-        if (effects.pingPong.active) {
-          var noEffects = false;
+            tuna = new Tuna(offlineAudioCtx);
+            var phaser = new tuna.Phaser({
+              rate: effects.phaser.rate,
+              depth: effects.phaser.depth,
+              feedback: effects.phaser.feedback,
+              stereoPhase: effects.phaser.stereoPhase,
+              baseModulationFrequency: effects.phaser.baseModulationFrequency
+            });
+            bufferSource.connect(phaser);
+            phaser.connect(offlineAudioCtx.destination);
+          }
 
-          var pingPongDelayNode = new tuna.PingPongDelay({
-              wetLevel: effects.pingPong.wetLevel,
-              feedback: effects.pingPong.feedback,
-              delayTimeLeft: effects.pingPong.delayTimeLeft,
-              delayTimeRight: effects.pingPong.delayTimeRight
-          });
-          bufferSource.connect(pingPongDelayNode);
-          pingPongDelayNode.connect(offlineAudioCtx.destination);
-        }
+          if (effects.convolver.active) {
+            var noEffects = false;
+            var convolver = new tuna.Convolver({
+              highCut: effects.convolver.highCut,
+              lowCut: effects.convolver.lowCut,
+              dryLevel: effects.convolver.dryLevel,
+              wetLevel: effects.convolver.wetLevel,
+              level: effects.convolver.level,
+              impulse: effects.convolver.impulse
+            });
+            bufferSource.connect(convolver);
+            convolver.connect(offlineAudioCtx.destination);
+          }
 
-        if (effects.phaser.active) {
-          var noEffects = false;
+          if(effects.wahwah.active) {
+            var noEffects = false;
+            var wahwah = new tuna.WahWah({
+                automode: effects.wahwah.automode,
+                baseFrequency: effects.wahwah.baseFrequency,
+                excursionOctaves: effects.wahwah.excursionOctaves,
+                sweep: effects.wahwah.sweep,
+                resonance: effects.wahwah.resonance,
+                sensitivity: effects.wahwah.sensitivity
+            });
+            bufferSource.connect(wahwah);
+            wahwah.connect(offlineAudioCtx.destination);
+          }
 
-          tuna = new Tuna(offlineAudioCtx);
-          var phaser = new tuna.Phaser({
-            rate: effects.phaser.rate,
-            depth: effects.phaser.depth,
-            feedback: effects.phaser.feedback,
-            stereoPhase: effects.phaser.stereoPhase,
-            baseModulationFrequency: effects.phaser.baseModulationFrequency
-          });
-          bufferSource.connect(phaser);
-          phaser.connect(offlineAudioCtx.destination);
-        }
+          if (noEffects) {
+            bufferSource.connect(offlineAudioCtx.destination);
+          }
 
-        if (effects.convolver.active) {
-          var noEffects = false;
-          var convolver = new tuna.Convolver({
-            highCut: this.effects.convolver.highCut,
-            lowCut: this.effects.convolver.lowCut,
-            dryLevel: this.effects.convolver.dryLevel,
-            wetLevel: this.effects.convolver.wetLevel,
-            level: this.effects.convolver.level,
-            impulse: this.effects.convolver.impulse
-          });
-          bufferSource.connect(convolver);
-          convolver.connect(offlineAudioCtx.destination);
-        }
-
-        if(effects.wahwah.active) {
-          var noEffects = false;
-          var wahwah = new tuna.WahWah({
-              automode: effects.wahwah.automode,
-              baseFrequency: effects.wahwah.baseFrequency,
-              excursionOctaves: effects.wahwah.excursionOctaves,
-              sweep: effects.wahwah.sweep,
-              resonance: effects.wahwah.resonance,
-              sensitivity: effects.wahwah.sensitivity
-          });
-          bufferSource.connect(wahwah);
-          wahwah.connect(offlineAudioCtx.destination);
-        }
-
-        if (noEffects) {
-          bufferSource.connect(offlineAudioCtx.destination);
-        }
-
-        // Kick off the render, callback will contain rendered buffer in event
-        offlineAudioCtx.startRendering();
-
-        // Render the databent image.
-        offlineAudioCtx.oncomplete = function (e) {
-          // Instead of starting the bufferSource, move data back into Canvas land.
-          _this.renderedBuffer = e.renderedBuffer;
-          _this.draw(e.renderedBuffer);
-        };
+          // Kick off the render, callback will contain rendered buffer in event
+          offlineAudioCtx.startRendering();
+          // Render the databent image.
+          offlineAudioCtx.oncomplete = function (e) {
+            resolve(e.renderedBuffer);
+          };
+        });
       };
 
       this.draw = function (buffer) {
@@ -206,14 +189,18 @@
 
         // putImageData requires an ImageData Object
         // @see https://developer.mozilla.org/en-US/docs/Web/API/ImageData
-        this.transformedImage = new ImageData(clampedDataArray, this.canvas.width, this.canvas.height)
+        var transformedImage = new ImageData(clampedDataArray, this.imageData.width, this.imageData.height);
+
         if (!document.querySelector('canvas')) {
-          document.body.prepend(this.canvas);
+          var canvas = document.createElement('canvas');
+          canvas.width = this.imageData.width;
+          canvas.height = this.imageData.height;
+          document.body.prepend(canvas);
         }
-        document.querySelector('canvas').getContext('2d').putImageData(this.transformedImage, 0, 0);
+
+        document.querySelector('canvas').getContext('2d').putImageData(transformedImage, 0, 0);
       };
 
-      this.render(this.imageData);
 
       return this;
     };
